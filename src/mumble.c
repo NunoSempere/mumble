7f8b1dfb2f5b39f125f10e3b1c336c909710c84b
NunoSempere <nuno.sempere@protonmail.com>
2023-04-29 19:08:38 -0400

    step: handle ctrl+D.

--- src/mumble.c
+++ src/mumble.c
@@ -8,10 +8,12 @@ int main(int argc, char** argv)
     puts("Press Ctrl+C to exit\n");
     int loop = 1;
     while (loop) {
-        fputs("mumble>", stdout);
+        fputs("mumble> ", stdout);
         void* catcher = fgets(input, 2048, stdin);
         if (catcher == NULL) {
+            // ^ catches Ctrl+D
             loop = 0;
+            puts("");
         } else {
             printf("You said: %s", input);
         }
