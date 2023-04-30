2ffb9e7d82bbda36b0b2c8b4856001a9b453ef6e
NunoSempere <nuno.sempere@protonmail.com>
2023-04-30 00:41:57 -0400

    step: start adding C mpc

--- makefile
+++ makefile
@@ -13,13 +13,25 @@ DEBUG=-g#-g
 
 ## Main file
 SRC=./src/mumble.c
+MPC=./src/mpc/mpc.c
+
+## Dependencies
+DEPS_PC=libedit
+# ^ libm, which doesn't have files which pkg-config can find, grr.
+DEBUG= #'-g'
+
+INCS=`pkg-config --cflags ${DEPS_PC}`
+LIBS_PC=`pkg-config --libs ${DEPS_PC}`
+LIBS_DIRECT=-lm
+LIBS=$(LIBS_DIRECT) $(LIBS_PC)
+# $(CC) $(DEBUG) $(INCS) $(PLUGS) $(SRC) -o rose $(LIBS) $(ADBLOCK)
 
 ## Formatter
 STYLE_BLUEPRINT=webkit
 FORMATTER=clang-format -i -style=$(STYLE_BLUEPRINT)
 
 build: $(SRC)
-	$(CC) $(SRC) -o mumble $(DEBUG) 
+	$(CC)  -Wall  $(INCS) $(SRC) $(MPC) -o mumble $(LIBS)
 
 format: $(SRC)
 	$(FORMATTER) $(SRC)
--- src/mumble.c
+++ src/mumble.c
@@ -1,22 +1,44 @@
+#include <editline/history.h>
+#include <editline/readline.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-static char input[2048];
+#include "mpc/mpc.h"
 
 int main(int argc, char** argv)
 {
     puts("Mumble version 0.0.1\n");
-    puts("Press Ctrl+C to exit\n");
+    puts("Press Ctrl+C/Ctrl+D to exit\n");
+
+    /* Create Some Parsers */
+    mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr = mpc_new("expr");
+    mpc_parser_t* Lispy = mpc_new("lispy");
+
+    /* Define them with the following Language */
+    mpca_lang(MPCA_LANG_DEFAULT,
+        "                                               \
+    number   : /-?[0-9]+/ ;                             \
+    operator : '+' | '-' | '*' | '/' ;                  \
+    expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+    lispy    : /^/ <operator> <expr>+ /$/ ;             \
+  ",
+        Number, Operator, Expr, Lispy);
     int loop = 1;
     while (loop) {
-        fputs("mumble> ", stdout);
-        void* catcher = fgets(input, 2048, stdin);
-        if (catcher == NULL) {
+        char* input = readline("mumble> ");
+        if (input == NULL) {
             // ^ catches Ctrl+D
             loop = 0;
             puts("");
         } else {
-            printf("You said: %s", input);
+            printf("You said: %s\n", input);
+            add_history(input);
+            // can't add if input is NULL
         }
+        free(input);
     }
+
     return 0;
 }
