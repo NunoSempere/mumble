14113fb6365d1420c9ae539ac23fc6124678166d
NunoSempere <nuno.sempere@protonmail.com>
2023-05-02 10:12:46 -0400

    feat: rework symbols regex.

--- src/mumble.c
+++ src/mumble.c
@@ -587,9 +587,7 @@ int main(int argc, char** argv)
     /* Define them with the following Language */
     mpca_lang(MPCA_LANG_DEFAULT, "                           \
     number   : /-?[0-9]+\\.?([0-9]+)?/ ;                     \
-    symbol : \"list\" | \"head\" | \"tail\"  \
-		       | \"eval\" | \"join\" | \"len\"   \
-           | '+' | '-' | '*' | '/' ;        \
+    symbol : /[a-zA-Z0-9_+\\-*\\/\\\\=<>!&]+/ ;         \
 		sexpr : '(' <expr>* ')' ;                                \
 		qexpr : '{' <expr>* '}' ;                                \
     expr     : <number> | <symbol> | <sexpr> | <qexpr>;      \
