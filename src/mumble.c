c145c862f92b375bc4c603b3481e2c1422608703
NunoSempere <nuno.sempere@protonmail.com>
2023-05-07 12:32:42 -0400

    feat: more scaffolding for functions!

--- src/mumble.c
+++ src/mumble.c
@@ -995,7 +995,7 @@ int main(int argc, char** argv)
     /* Define them with the following Language */
     mpca_lang(MPCA_LANG_DEFAULT, "                           \
     number   : /-?[0-9]+\\.?([0-9]+)?/ ;                     \
-    symbol : /[a-zA-Z0-9_+\\-*\\/\\\\=<>!&]+/ ;         \
+    symbol : /[a-zA-Z0-9_+\\-*\\/\\\\=<>!&@]+/ ;         \
 		sexpr : '(' <expr>* ')' ;                                \
 		qexpr : '{' <expr>* '}' ;                                \
     expr     : <number> | <symbol> | <sexpr> | <qexpr>;      \
